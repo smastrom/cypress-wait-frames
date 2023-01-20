73869332a5b9877bc89bb8860894d3c078f2a523
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2023-01-20 22:45:59 +0100

    window tests ok

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,24 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+import '../../src/index';
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace Cypress {
+		interface Chainable {
+			expectError(done: () => void, message: string): boolean;
+		}
+	}
+}
+
+function normalize(str: string): string {
+	return str.replace(/\s+/g, ' ');
+}
+
+Cypress.Commands.add('expectError', (end: Mocha.Done, message) => {
+	cy.on('fail', (err) => {
+		expect(normalize(err.message)).to.eq(normalize(message));
+		end(); // Complete
+		return false; // Prevent failure
+	});
+});
--- tests/App.cy.tsx
+++ tests/App.cy.tsx
@@ -1,7 +1,7 @@
 import { App } from './App';
 
 describe('Window', () => {
-	it('Should wait for scrollY', () => {
+	it('Single Property', () => {
 		cy.mount(<App />);
 		cy.scrollTo('bottom');
 
@@ -24,6 +24,38 @@ describe('Window', () => {
 			frames: 30,
 		}).then((data) => {
 			expect(data[0].value).to.equal(0);
+		});
+	});
+
+	it('Multiple Properties', () => {
+		cy.mount(<App />);
+		cy.scrollTo('bottom');
+
+		cy.waitFrames({
+			subject: () => cy.window(),
+			property: ['scrollY', 'pageYOffset'],
+			frames: 30,
+		}).then((data) => {
+			expect(data.length).to.equal(2);
+
+			cy.document().then((doc) => {
+				const scrollHeight = doc.documentElement.scrollHeight - doc.documentElement.clientHeight;
+				expect(data[0].value).to.equal(scrollHeight);
+				expect(data[1].value).to.equal(scrollHeight);
+			});
+		});
+	});
+
+	it('Should throw error if unknown prop', (end) => {
+		cy.mount(<App />);
+		cy.scrollTo('bottom');
+
+		cy.expectError(end, '[cypress-wait-frames] - Invalid window property: background-color');
+
+		cy.waitFrames({
+			subject: () => cy.window(),
+			property: 'background-color',
+			frames: 30,
 		});
 	});
 });
