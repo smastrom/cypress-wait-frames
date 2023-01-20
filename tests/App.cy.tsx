6ab5c7fe04f00d65386c93a62a1ec324924feb72
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2023-01-20 22:32:17 +0100

    add first test

--- tests/App.cy.tsx
+++ tests/App.cy.tsx
@@ -1,5 +1,29 @@
 import { App } from './App';
 
-it('App', () => {
-	cy.mount(<App />);
+describe('Window', () => {
+	it('Should wait for scrollY', () => {
+		cy.mount(<App />);
+		cy.scrollTo('bottom');
+
+		cy.waitFrames({
+			subject: () => cy.window(),
+			property: 'scrollY',
+			frames: 30,
+		}).then((data) => {
+			cy.document().then((doc) => {
+				const scrollHeight = doc.documentElement.scrollHeight - doc.documentElement.clientHeight;
+				expect(data[0].value).to.equal(scrollHeight);
+			});
+		});
+
+		cy.scrollTo('top');
+
+		cy.waitFrames({
+			subject: () => cy.window(),
+			property: 'scrollY',
+			frames: 30,
+		}).then((data) => {
+			expect(data[0].value).to.equal(0);
+		});
+	});
 });
