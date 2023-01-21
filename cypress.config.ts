69b78285767d7b42d7ddf8c643636f4d9c568244
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2023-01-21 11:08:17 +0100

    add tests gh workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
 	component: {
 		viewportWidth: 1280,
 		viewportHeight: 720,
+		experimentalWebKitSupport: true,
 		devServer: {
 			framework: 'react',
 			bundler: 'vite',
